fa9d3e8f75408baf7a0fb271e06410c76e7b03ba
Roberto Ho <ippoho0705@gmail.com>
2020-05-28 16:03:45 +0800

    changed HelloEclipse.java

---
+++ HelloEclipse.java
@@ -0,0 +1,12 @@
+package xxx;
+
+public class HelloEclipse {
+	// This is the beginning
+	/**********************
+	 *                    * 
+	 *                    * 
+	 **********************/
+	public static void main(String[] Args) {
+		System.out.println("歡迎使用Eclipse^_^");
+	}
+}
